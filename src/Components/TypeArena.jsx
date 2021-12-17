36e286cebdf1b751aa4655b390bac7529a94bfbd
Prerit <myalternateid44@gmail.com>
2021-12-17 21:20:09 +0530

    initialized project

---
+++ src/Components/TypeArena.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Styles/TypeArena.css";
+
+const TypeArena = () => {
+    return (
+        <div className='TypeArena'>
+            
+        </div>
+    )
+}
+
+export default TypeArena;
